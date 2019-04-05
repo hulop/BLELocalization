81f2a21b2d041d77094160d19b7852b8d9f08377
Takashi Itoh <jl03313@jp.ibm.com>
2019-04-05 14:24:26 +0900

    Update download script, project facet and classpath files

--- download-lib.sh
+++ download-lib.sh
@@ -12,16 +12,17 @@ curl -O https://code.jquery.com/jquery-1.11.2.min.js
 cd ..
 
 curl -L -O https://github.com/openlayers/ol2/releases/download/release-2.13.1/OpenLayers-2.13.1.zip
-unzip OpenLayers-2.13.1.zip
+unzip -o -q OpenLayers-2.13.1.zip && rm OpenLayers-2.13.1.zip
 
 
 curl -L -O https://jqueryui.com/resources/download/jquery-ui-1.11.4.zip
-unzip jquery-ui-1.11.4.zip
+unzip -o -q jquery-ui-1.11.4.zip && rm jquery-ui-1.11.4.zip
 
 curl -L -O https://github.com/DataTables/DataTables/archive/1.10.6.zip
-unzip 1.10.6.zip
+unzip -o -q 1.10.6.zip && rm 1.10.6.zip
 
 cd $pwd
+mkdir LocationService/WebContent/WEB-INF/lib
 cd LocationService/WebContent/WEB-INF/lib
 
 curl -L -O https://github.com/mongodb/mongo-java-driver/releases/download/r2.12.0/mongo-java-driver-2.12.0.jar
@@ -30,9 +31,9 @@ cd $pwd
 cd location-service-library/lib
 
 curl -L -O http://archive.apache.org/dist/commons/math/binaries/commons-math3-3.3-bin.zip
-unzip commons-math3-3.3-bin.zip commons-math3-3.3/commons-math3-3.3.jar
+unzip commons-math3-3.3-bin.zip commons-math3-3.3/commons-math3-3.3.jar && rm commons-math3-3.3-bin.zip
 mv commons-math3-3.3/commons-math3-3.3.jar ./
 
-curl -L -O http://ftp.wayne.edu/apache/wink/1.4.0/apache-wink-1.4.zip
-unzip apache-wink-1.4.zip apache-wink-1.4/dist/wink-1.4.jar
+curl -L -O http://archive.apache.org/dist/wink/1.4.0/apache-wink-1.4.zip
+unzip apache-wink-1.4.zip apache-wink-1.4/dist/wink-1.4.jar && rm apache-wink-1.4.zip
 mv apache-wink-1.4/dist/wink-1.4.jar ./
